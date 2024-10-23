bafec8d5c1bcde76e350a47b4a3b5778f6d9e633
npcuong <phucuonggk@gmai.com>
2024-10-23 17:24:21 +0700

    assignment2

--- Assignment1/Solution1.cpp
+++ Assignment1/Solution1.cpp
@@ -1,65 +1,41 @@
-﻿#include<math.h>
-#include<iostream>
+﻿#include<iostream>
+#include <vector>
 using namespace std;
 
-
-
-int songuyento(int n) {
-	for (int i = 2; i <= sqrt(n); i++) {
-		if (n % i == 0) {
-			return 0;
-		}
-	}
-	return 1;
+void swap(int& a, int & b) {
+	int temp = a;
+	a = b;
+	b = temp;
 }
 
 
+void selectionSort(int arr[], int n) {
+	int i, j, min_indx;
 
-int soNUAnguyento(int n) {
-	for (int i = 2; i < n; i++) {
-		for (int j = 2; j < n; j++) {
-			if (songuyento(i) && songuyento(j) == 1) {
-				if (i * j == n) {
-					return 1;
-				}
-			}
-		}
+	for (i = 0; i < n - 1;i++)
+	{
+		min_indx = i;
+		for (j = i + 1; j < n;j++)
+			if (arr[j] < arr[min_indx])
+				min_indx = j;
+		swap(arr[min_indx], arr[i]);
+
 	}
-	return 0;
 }
+
+// đỏ = 0 
+// trắng = 1
+// xanh = 2
+
 int main() {
-	int a[100], blumNumber[100];
-	int count = 0;
-	for (int i = 0;i < 100;i++) {
-		a[i] = i + 1;
+	int arr[] = {2,0,2,0,0,1,0,2,1};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	selectionSort(arr, n);
+	cout << "Consult: \n";
+	for (int i = 0; i < n;i++) {
+		cout << arr[i] << " ";
 	}
-	for (int i = 0;i < 100;i++) {
-		if (soNUAnguyento(a[i]) == 1) {
-			blumNumber[count++] = a[i];
-		}
-	}
-	for (int i = 0;i < count;i++) {
-		for (int j = 0;j < count;j++) {
-			int sum = blumNumber[j] + blumNumber[i];
-			for (int k = 0;k < count;k++) {
-				if (sum == blumNumber[k]) { 
-					cout << "(" << blumNumber[i] << ", " << blumNumber[j] << ")" << endl;
-				}
-			}
-		}
-	}
-	int M;
-	bool check = false;
-	cout << "nhap M:";
-	cin >> M;
-	for (int i = 0;i < count;i++) {
-		if (M == blumNumber[i]) {
-			check = true;
-		}
-	}
-	if (check == true) {
-		cout << "M co ton tai trong day Blum" << endl;
-	}
-	else cout << "khong ton tai";
+	cout << endl;
+	return 0;
 }
 
---
+++ Assignment2/Source.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+
+	const int NUM_YEARS = 51; // Số lượng năm
+	int population[NUM_YEARS];
+
+	// Nhập dữ liệu
+
+	for (int i = 0; i < NUM_YEARS; ++i) {
+		cout << "Nhap so luong nguoi sinh cua nam " << 1920 + i << ": " << endl;
+		cin >> population[i];
+	}
+
+	// Tìm số năm có cùng số lượng người sinh
+	int count_same_population = 0;
+	for (int i = 0; i < NUM_YEARS - 1; ++i) {
+		for (int j = i + 1; j < NUM_YEARS; ++j) {
+			if (population[i] == population[j]) {
+				count_same_population++;
+			}
+		}
+	}
+
+	// Tìm năm có lượng người sinh lớn nhất và nhỏ nhất
+	int max_population = population[0], min_population = population[0];
+	int max_year = 1920, min_year = 1920;
+	for (int i = 1; i < NUM_YEARS; ++i) {
+		if (population[i] > max_population) {
+			max_population = population[i];
+			max_year = 1920 + i;
+		}
+		if (population[i] < min_population) {
+			min_population = population[i];
+			min_year = 1920 + i;
+		}
+	}
+
+	// In kết quả
+
+	cout << "So nam co cung so luong nguoi sinh: " << count_same_population << endl;
+	cout << "Nam co luong nguoi sinh lon nhat: " << max_year << " (" << max_population << ")" << endl;
+	cout << "Nam co luong nguoi sinh nho nhat: " << min_year << " (" << min_population << ")" << endl;
+
+
+
+	return 0;
+
+}
